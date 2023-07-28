9a674a90f93d5fa424834bfc46377a663ba0b0e7
HrytsenkoNatalia <natalia.hrytsenko@gmail.com>
2023-07-28 21:15:45 +0300

    Making changes to the tests (project task 2)

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,26 +1,11 @@
-class User:
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
+import pytest
 
-    def create(self):
-        self.name = "Sergii"
-        self.second_name = "Butenko"
 
-    def remove(self):
-        self.name = ""
-        self.second_name = ""
+@pytest.mark.check
+def test_change_name(user):
+    assert user.name == "Natalia"
 
-    def test_change_name():
-        user = User()
-        user.create()
 
-        assert user.name == "Sergii"
-        user.remove()
-
-    def test_change_second_name():
-        user = User()
-        user.create()
-
-        assert user.second_name == "Butenko"
-        user.remove()
+@pytest.mark.check
+def test_change_second_name(user):
+    assert user.second_name == "Hrytsenko"
